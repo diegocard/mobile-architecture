57afac36b74eab37086ab7935932d992dc4bf6aa
gonzalomelov <gonzalomelov@gmail.com>
2013-05-20 00:03:42 -0300

    Sucursales CRUD completo

--- HomeBanking/HomeBanking-web/src/main/java/org/fing/tagsi/grupo8/homebanking/sl/services/SucursalesResource.java
+++ HomeBanking/HomeBanking-web/src/main/java/org/fing/tagsi/grupo8/homebanking/sl/services/SucursalesResource.java
@@ -1,11 +1,17 @@
 package org.fing.tagsi.grupo8.homebanking.sl.services;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import org.fing.tagsi.grupo8.homebanking.common.entities.Sucursal;
 import org.fing.tagsi.grupo8.homebanking.pl.serviceagent.SucursalesSA;
@@ -19,9 +25,26 @@ public class SucursalesResource {
 
     @GET
     @Produces("application/json")
-    public List<Sucursal> getJson() {
+    public List<Sucursal> getSucursales() {
         return SucursalesSA.getSucursales().getAllSucursales();
     }
     
+    @Path("{idSucursal}")
+    @GET
+    @Produces("application/json")
+    public Sucursal getSucursal(@PathParam("idSucursal") int idSucursal) {
+        return SucursalesSA.getSucursales().getSucursal(new Long(idSucursal));
+    }
+    
+    @POST
+    public void addSucursal(Sucursal sucursal) {
+        SucursalesSA.getSucursales().addSucursal(sucursal);
+    }
+    
+    @Path("{idSucursal}")
+    @DELETE
+    public void removeSucursal(@PathParam("idSucursal") int idSucursal) {
+        SucursalesSA.getSucursales().removeSucursal(new Long(idSucursal));
+    }
     
 }
