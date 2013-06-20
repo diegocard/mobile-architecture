848c88358a79fc1f9e2434b2a58e5fb9e9e91b41
diegocard <diegocard@gmail.com>
2013-06-20 03:10:10 -0300

    Avances en el cliente

--- HTML5Application/public_html/logica.js
+++ HTML5Application/public_html/logica.js
@@ -1,9 +1,11 @@
 /* Variables */
-var URLBase = "http://tagsi.jelastic.servint.net:8080/HomeBanking-web/rest/";
-var Usuario =  null;
+var URLBase = "http://tagsi2013.jelastic.servint.net/HomeBanking-web/rest/";
+var IdUsuario =  null;
+var NomUsuario =  null;
 var Password = null;
 var IsAdmin = false;
 var UsuarioLogueado = false; //vale false hasta que el usuario se loguea
+var Cuentas = null; //Cuentas del usuario
 
 /* ============================== Inicio de la aplicacion ============================== */
 
@@ -12,6 +14,11 @@ $(document).bind('pageinit', eventosInicio);
 function eventosInicio(){
     //Eventos de usuario
 	$('#btn-login').bind('tap', btnLogin);
+    $('#btn-transferencia').bind('tap', btnTransferencia);
+    //Eventos al cargar la pagina de iniciobtnTransferencia
+    $(document).on('pagebeforecreate', '#page-inicio', eventosPageInicio);
+    //Eventos al cargar la pagina de inicio
+    $(document).on('pagebeforecreate', '#page-sucursales', eventosPageSucursales);
     //Invocamos a la persistencia para cargar los datos iniciales de la aplicacion    
     cargarBDInicial();
     //En caso de que exista un usuario persistente, realizo login con ese usuario    
@@ -31,26 +38,16 @@ function loginPersistente(){
     cargarUsuarioPersistente();
 }
 
-function loginSuccess(data){
-    if (data == true){
-        UsuarioLogueado = true;
-        guardarUsuarioPersistente(Usuario, Password, IsAdmin);
-        redirectToPage('#page-inicio');
-    }else{
-        alert('Username or password are incorrect');
-    }
-}
-
 function btnLogin(){
-    Usuario = $("#input-user").val();
+    NomUsuario = $("#input-user").val();
     Password = $("#input-password").val();
     IsAdmin = $("#lbl-is-admin").hasClass('ui-checkbox-on');
     login();    
 }
 
 function login(){
-    //Reliza login con los datos ya cargados en las variables Usuario, Password e IsAdmin
-    var url=URLBase + 'usuarios/validar/jsonp/' + Usuario + '/' + Password + '/' + IsAdmin;
+    //Realiza login con los datos ya cargados en las variables Usuario, Password e IsAdmin
+    var url=URLBase + 'usuarios/validar/jsonp/' + NomUsuario + '/' + Password + '/' + IsAdmin;
     $.ajax({
         url: url,
         dataType: 'jsonp',
@@ -58,3 +55,108 @@ function login(){
         jsonp: 'callback',
     });
 }
+
+function loginSuccess(ID){
+    if (ID != -1){
+        UsuarioLogueado = true;
+        IdUsuario = ID;
+        guardarUsuarioPersistente(IdUsuario, NomUsuario, Password, IsAdmin);
+        redirectToPage('#page-inicio');
+    }else{
+        alert('Username or password are incorrect');
+    }
+}
+
+function eventosPageInicio(){
+    //Carga las cuentas del usuario
+    if (IdUsuario != null){
+        var url=URLBase + 'cuentas/jsonp/usuario/' + IdUsuario;
+        $.ajax({
+            url: url,
+            dataType: 'jsonp',
+            jsonpCallback: 'cargarCuentasSuccess',
+            jsonp: 'callback',
+        });
+    }    
+}
+
+function cargarCuentasSuccess(data){
+    //Si tengo nuevos datos de cuenta, los guardo
+    if (data != null){
+        Cuentas = data;       
+    }
+    //Cargo la cuenta en el listado de inicio y de transferencias
+    $.each(Cuentas, function(index, cuenta){
+        cargarCuentaListadoInicio(cuenta);
+        cargarCuentaListadosTransferencia(cuenta);
+    });
+    $('#dividerCuentas').listview();
+    $('#dividerCuentas .btnDetaleCuenta').bind('tap', cargarDetalleCuenta);
+
+}
+
+function cargarCuentaListadoInicio(cuenta){
+    $('#dividerCuentas').append(
+        '<li data-theme="c">' +
+            '<a id="' + cuenta.id + '" href="#page-detalle-cuenta" data-transition="slide" class="btnDetaleCuenta">' + 
+                cuenta.tipo + ' ' + cuenta.numero +
+            '</a>' +
+        '</li>'
+    );
+}
+
+function cargarCuentaListadosTransferencia(cuenta){
+    $('#selectTransferenciaDesde').append(
+        '<option value="' + cuenta.id + '">' +
+            cuenta.tipo + ' ' + cuenta.numero +
+        '</option>'
+    );
+
+    $('#selectTransferenciaHasta').append(
+        '<option value="' + cuenta.id + '">' +
+            cuenta.tipo + ' ' + cuenta.numero +
+        '</option>'
+    );
+}
+
+
+function eventosPageSucursales(){
+    //Carga las cuentas del usuario
+    if (IdUsuario != null){
+        var url=URLBase + 'sucursales';
+        $.ajax({
+            url: url,
+            dataType: 'jsonp',
+            jsonpCallback: 'cargarSucursalesSuccess',
+            jsonp: 'callback',
+        });
+    }    
+}
+
+function cargarDetalleCuenta(){
+    var IDCuentaSeleccionada = $(this).attr('id');
+    if (Cuentas != null){
+        $.each(Cuentas, function(index, cuenta){
+            if (cuenta.id == IDCuentaSeleccionada){
+                $('#txtCuentaNumero').html(cuenta.numero);
+                $('#txtCuentaTipo').html(cuenta.tipo);
+                $('#txtCuentaMonto').html(cuenta.saldo);
+            }            
+        });
+    }
+}
+
+function cargarSucursalesSuccess(data){
+    if (data != null){
+        $.each(data, function(index, entrada){
+            alert(index + ' ' + data);
+        });
+    }
+}
+
+function btnTransferencia(){
+    var CuentaDesde = $('#selectTransferenciaDesde').find(":selected").text();
+    alert(CuentaDesde);
+    redirectToPage('#page-inicio');
+}
+
--- HTML5Application/public_html/persistencia.js
+++ HTML5Application/public_html/persistencia.js
@@ -1,5 +1,6 @@
 /* ========================== Manejo de tablas ========================== */
 var BDCargada = false;
+var VaciarBD = false;
 
 //Ejecuta las transacciones pendientes y devuelve la base
 function obtenerBD(){
@@ -9,7 +10,9 @@ function obtenerBD(){
 // Invoca la creacion de las tablas de la base de datos en caso de que no existan
 function cargarBDInicial() {
 	if (!BDCargada){
-		//obtenerBD().transaction(eliminarTablas, errorBD);	
+		if (VaciarBD){
+			obtenerBD().transaction(eliminarTablas, errorBD);
+		}
 		obtenerBD().transaction(iniciarTablaUsuario, errorBD);
 		//guardarUsuarioPersistente('asd','das');
 		BDCargada = true;
@@ -19,9 +22,9 @@ function cargarBDInicial() {
 // Elimina todas las tablas de la base
 function eliminarTablas(tx){
 	tx.executeSql('DROP TABLE IF EXISTS Cuenta');
-	tx.executeSql('DROP TABLE IF EXISTS Transferencia');	
-	tx.executeSql('DROP TABLE IF EXISTS Sucursal');
+	tx.executeSql('DROP TABLE IF EXISTS Transferencia');
 	tx.executeSql('DROP TABLE IF EXISTS Usuario');
+	tx.executeSql('DROP TABLE IF EXISTS Historial');
 }
 
 // Imprime errores de la base
@@ -34,11 +37,6 @@ function iniciarTablaCuenta(tx) {
 	tx.executeSql('CREATE TABLE IF NOT EXISTS Cuenta (ID unique, Numero, Saldo, Tipo, Usuario_ID)');	
 }
 
-// Crea la tabla de sucursales en caso de que no exista 
-function iniciarTablaSucursal(tx) {
-	tx.executeSql('CREATE TABLE IF NOT EXISTS Sucursal (ID unique, Direccion, Nombre, X, Y)');	
-}
-
 // Crea la tabla de transferencias en caso de que no exista 
 function iniciarTablaTransferencia(tx) {
 	tx.executeSql('CREATE TABLE IF NOT EXISTS Transferencia (ID unique, Descripcion, Monto, Cuenta_Destino_ID, Cuenta_Origen_ID)');	
@@ -46,7 +44,12 @@ function iniciarTablaTransferencia(tx) {
 
 // Crea la tabla que guarda los datos del usuario (unico)
 function iniciarTablaUsuario(tx) {
-	tx.executeSql('CREATE TABLE IF NOT EXISTS Usuario (Usuario, Password, IsAdmin)');	
+	tx.executeSql('CREATE TABLE IF NOT EXISTS Usuario (ID unique, Nombre, Password, IsAdmin)');	
+}
+
+// Crea la tabla que guarda los datos del historial
+function iniciarTablaUsuario(tx) {
+	tx.executeSql('CREATE TABLE IF NOT EXISTS Historial (IDUsuario, Mensaje)');	
 }
 
 /* ========================== Manejo de datos ========================== */
@@ -61,8 +64,9 @@ function cargarUsuarioPersistente(){
 				[], 
 				function (tx, resultado){
 					var CantFilas = resultado.rows.length;					
-					if (CantFilas > 0){								
-						Usuario = resultado.rows.item(0).Usuario;
+					if (CantFilas > 0){
+						IdUsuario = resultado.rows.item(0).ID;								
+						NomUsuario = resultado.rows.item(0).Nombre;
 						Password = resultado.rows.item(0).Password;
 						IsAdmin = resultado.rows.item(0).IsAdmin;
 						login();
@@ -75,7 +79,7 @@ function cargarUsuarioPersistente(){
 	);
 }
 
-function guardarUsuarioPersistente(User, Pass, IsAdm){
+function guardarUsuarioPersistente(ID, Name, Pass, IsAdm){
 	//Solo puede haber 1 usuario persistente
 	//En caso de que haya alguno guardado, lo elimino
 	var queryDelete = 'DELETE FROM USUARIO';
@@ -87,154 +91,13 @@ function guardarUsuarioPersistente(User, Pass, IsAdm){
 		errorBD
 	);
 	//Guardo el nuevo usuario persistente
-	var queryInsert = 'INSERT INTO Usuario (Usuario, Password, IsAdmin) ' +
-				'VALUES ("' + User + '", "' + Pass + '", "' + IsAdm + '")';
+	var queryInsert = 'INSERT INTO Usuario (ID, Nombre, Password, IsAdmin) ' +
+				'VALUES ("' + ID + '", "' + Name + '", "' + Pass + '", "' + IsAdm + '")';
 	var db = obtenerBD();
 	db.transaction(
 		function(tx){
 			tx.executeSql(queryInsert);
 		},
-		errorBD
-	);
-}
-
-function guardarFavoritoConChequeo(ID, Nombre, Direccion, Telefonos, Mail, Web){
-	var db = obtenerBD();
-	var query = 'SELECT * FROM Favoritos WHERE ID="' + ID + '"';
-	db.transaction(
-		function(tx){
-			tx.executeSql(
-				query, 
-				[], 
-				function (tx, resultado){
-					var CantFilas = resultado.rows.length;					
-					if (CantFilas == 0){						
-						guardarFavorito(ID, Nombre, Direccion, Telefonos, Mail, Web);
-					}					
-				}, 
-				errorBD
-			);
-		},
-		errorBD
-	);
-}
-
-function guardarFavorito(ID, Nombre, Direccion, Telefonos, Mail, Web){	
-	var db = obtenerBD();
-	var query = 'INSERT INTO Favoritos (ID, Nombre, Direccion, Telefonos, Mail, Web) ' +
-			'VALUES ("' + ID + '", "' + Nombre + '", "' + Direccion + '", "' + Telefonos + '", "' + Mail + '", "' + Web + '")';
-	db.transaction(
-		function(tx){tx.executeSql(query);},
-		errorBD
-	);
-}
-
-function eliminarFavorito(ID){
-	var db = obtenerBD();
-	var query = 'DELETE FROM Favoritos WHERE ID="' + ID + '"';
-	db.transaction(
-		function(tx){tx.executeSql(query);}, 
-		errorBD
-	);	
-}
-
-function mostrarBotonEliminarSiExiste(id){
-	var db = obtenerBD();
-	var query = 'SELECT * FROM Favoritos WHERE ID="' + id + '"';
-	db.transaction(
-		function(tx){
-			tx.executeSql(
-				query, 
-				[], 
-				function (tx, resultado){
-					var CantFilas = resultado.rows.length;
-					if (CantFilas > 0){
-						mostrarBotonEliminarFavoritos();
-					}else{
-						mostrarBotonAgregarFavoritos();
-					}					
-				}, 
-				errorBD
-			);
-		},
-		errorBD
-	);
-}
-
-function mostrarBD(){
-	var db = obtenerBD();
-	db.transaction(
-		function(tx){
-			tx.executeSql(
-				'SELECT * FROM Favoritos;', 
-				[], 
-				function(tx, resultado){ 
-					var mensaje = '';
-					var CantFilas = resultado.rows.length;
-					mensaje += "Tabla Favoritos: " + CantFilas + " filas encontradas. \n";
-					for (var i=0; i<CantFilas; i++){
-						mensaje += "Fila = " + i + " ID = " + resultado.rows.item(i).ID + " Nombre =  " + resultado.rows.item(i).Nombre + "\n";
-					}
-					alert(mensaje);	
-				}, 
-				errorBD
-			);
-		}, 
-		errorBD
-	);
-}
-
-function accionCargarFavoritos(){
-	var db = obtenerBD();
-	db.transaction(
-		function(tx){
-			$("#lstFavoritos").html("");
-			tx.executeSql(
-				'SELECT * FROM Favoritos;', 
-				[], 
-				function(tx, resultado){ 
-					var CantFilas = resultado.rows.length;
-					for (var i=0; i<CantFilas; i++){
-						var insert=
-							"<li data-theme='b'>" +
-								"<a href='#InfoOperador' data-transition='slide' id=\"" + resultado.rows.item(i).ID + "\">" +
-									resultado.rows.item(i).Nombre +
-								"</a>" +
-							"</li>";
-						$("#lstFavoritos").append(insert);
-					}
-					$("#lstFavoritos").listview('refresh');
-				}, 
-				errorBD
-			);
-		}, 
-		errorBD
-	);
-}
-
-function tapFavorito(){
-	var index = $(this).index();
-	indiceFavoritoSeleccionado = index;	
-	var db = obtenerBD();
-	//Busco el favorito seleccionado en la BD
-	db.transaction(
-		function(tx){
-			$("#lstFavoritos").html("");
-			tx.executeSql(
-				'SELECT * FROM Favoritos;', 
-				[], 
-				function(tx, resultado){
-					var ID = resultado.rows.item(index).ID;
-					var Nombre = resultado.rows.item(index).Nombre;
-					var Direccion = resultado.rows.item(index).Direccion;
-					var Telefonos = resultado.rows.item(index).Telefono;
-					var Mail = resultado.rows.item(index).Mail;
-					var Web = resultado.rows.item(index).Web;
-					cargarPaginaOperador(ID, Nombre, Direccion, Telefonos, Mail, Web);					
-				}, 
-				errorBD
-			);
-		}, 
 		errorBD
 	);
 }
